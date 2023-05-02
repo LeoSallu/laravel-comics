8ca23c159583268eacf12f4e53f2baccc92ad723
LeoSallu <L.sallustio99@gmail.com>
2023-05-02 16:49:08 +0200

    migliorato layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,9 @@
     <div id="jumbotron">
         <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="logo" srcset="">
     </div>
-    <div class="background">
         <div class="container">
-            <div class="button p-3">
+            <div class="button p-3 w-25">
                 <h4>CURRENT SERIES</h4>
             </div>
-            <div class="thumbs">
-                
-            </div>
-            <div class="button p-3">
-                <h4>LOAD MORE</h4>
-            </div>
         </div>
-    </div>
 @endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,11 +3,23 @@
 @section('page.main')
     <section>
         <div class="container">
-            <ul>
-                @foreach ($comics as $comic)
-                    <li>{{ $comic['title'] }}</li>
-                @endforeach
-            </ul>
+            <div class="row">
+                <div class="d-flex flex-wrap justify-content-center">
+                    @foreach ($comics as $comic)
+                        <div id="thumbs" class="card m-3" style="width: 18rem;">
+                            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $comic['title'] }}</h5>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <div class="button my-2 p-3 w-25">
+                        <p class="text-center my-1">LOAD MORE</p>
+                    </div>                            
+                </div>
+            </div>
         </div>
     </section>
 @endsection
